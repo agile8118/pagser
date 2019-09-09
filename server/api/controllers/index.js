ece27ea034b48a386dfd9406a2775ebf23a9ab7f
agile8118 <agile.8118@usa.com>
2019-09-09 23:32:41 +0430

    Add footer and changing theme logic

--- server/api/controllers/index.js
+++ server/api/controllers/index.js
@@ -4,43 +4,43 @@ const User = require("../../models/user");
 const util = require("../../lib/util");
 
 exports.fetchHomePages = function(req, res) {
-	var homePages = {
-		newsest: [],
-		reviewed: [],
-		mostPopuler: [],
-		viewed: []
-	};
+  var homePages = {
+    newsest: [],
+    reviewed: [],
+    mostPopuler: [],
+    viewed: []
+  };
 
-	function fetchNewestPages(cb) {
-		Page.find(
-			{ type: "public" },
-			"_id contents.title contents.briefDes featureImage url date author",
-			function(err, pages) {
-				if (err) {
-					return res.send("error");
-				}
-				homePages.newsest = pages;
-				cb();
-			}
-		)
-			.lean()
-			.limit(5)
-			.sort({ date: -1 });
-	}
+  function fetchNewestPages(cb) {
+    Page.find(
+      { type: "public" },
+      "_id contents.title contents.briefDes featureImage url date author",
+      function(err, pages) {
+        if (err) {
+          return res.send("error");
+        }
+        homePages.newsest = pages;
+        cb();
+      }
+    )
+      .lean()
+      .limit(5)
+      .sort({ date: -1 });
+  }
 
-	fetchNewestPages(function() {
-		res.send(homePages);
-	});
+  fetchNewestPages(function() {
+    res.send(homePages);
+  });
 };
 
 exports.getAuth = (req, res) => {
-	if (req.user.id) {
-		User.findById(req.user.id, "photo", (err, user) => {
-			res
-				.status(200)
-				.send({ user: { id: user.id, photo: user.photo.secure_url } });
-		});
-	} else {
-		res.status(400).send();
-	}
+  if (req.user.id) {
+    User.findById(req.user.id, "photo", (err, user) => {
+      res
+        .status(200)
+        .send({ user: { id: user.id, photo: user.photo.secure_url } });
+    });
+  } else {
+    res.status(400).send();
+  }
 };
