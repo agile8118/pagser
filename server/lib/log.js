708ee2c94039afb6c982f04b5f8e16c8e03fa84a
Agile <agile.8118@usa.com>
2020-07-29 02:04:59 +0430

    Add some minor improvements in the logging system

--- server/lib/log.js
+++ server/lib/log.js
@@ -1,6 +1,10 @@
 const fs = require("fs");
 
+/* 
+  if the message parameter is object we log error and if it's string we log info
+*/
 module.exports = (message) => {
+  // Format the current date to use for each log
   const d = new Date();
   const dateString =
     d.getUTCFullYear() +
@@ -15,8 +19,12 @@ module.exports = (message) => {
     ":" +
     ("0" + d.getUTCSeconds()).slice(-2);
 
+  // Base log directory
   let dir = "./logs";
+  // If we don't have logs folder, create it
   if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+  // Create folders in log folder for better log organization based on each day
   dir =
     dir +
     "/" +
@@ -27,6 +35,7 @@ module.exports = (message) => {
     ("0" + d.getUTCDate()).slice(-2);
   if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
+  // If we either pass an object or specify the second parameter as error
   if (typeof message === "object") {
     const errorStr = JSON.stringify(message, [
       "message",
@@ -39,23 +48,20 @@ module.exports = (message) => {
     const errStack = JSON.parse(errorStr).stack
       ? "\n" + "Error Stack:\n" + JSON.parse(errorStr).stack
       : "";
-
     const errMessage = JSON.parse(errorStr).message
       ? "\n" + "Error Message: " + JSON.parse(errorStr).message
       : "";
-
     const errArguments = JSON.parse(errorStr).arguments
       ? "\n" + "Error Arguments: " + JSON.parse(errorStr).arguments
       : "";
-
     const errType = JSON.parse(errorStr).type
       ? "\n" + "Error Type: " + JSON.parse(errorStr).type
       : "";
-
     const errName = JSON.parse(errorStr).name
       ? "\n" + "Error Name: " + JSON.parse(errorStr).name
       : "";
 
+    // Prepare the message to be logged
     const msg =
       "---------------------------------------\n" +
       dateString +
@@ -66,13 +72,17 @@ module.exports = (message) => {
       errStack +
       "\n" +
       "---------------------------------------\n";
+    // Add the log message to errors.log file in the right directory
     fs.appendFile(dir + "/errors.log", msg, function (err) {
       if (err) return console.error(err);
     });
   }
 
+  // If we pass a string to the function
   if (typeof message === "string") {
+    // Prepare the message to be logged
     const msg = dateString + " -- " + message + "\n";
+    // Add the message to info.log file in the right directory
     fs.appendFile(dir + "/info.log", msg, function (err) {
       if (err) return console.error(err);
     });
